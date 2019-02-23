f10b0cde082721cfd60ec822ab809a27b1983b20
park-keon-kuk <pkk1113@naver.com>
2019-02-23 22:17:00 +0900

    express webpack-dev-middleware 설치

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# 3장 Output Management
+# 4장 Development
 
 ----------
+
+>3장 Output Management에서 이어집니다.
 
 ## 실습에 필요한 설치 파일들
 
 ```bash
-npm i -D webpack-dev-server
+npm i -D express webpack-dev-middleware
 ```
 
-> 1장에서 미리 설치하여 설치할 필요없습니다.
+> 1장에서 webpack-dev-server는 미리 설치하여 설치할 필요없습니다.
 > i는 install, -D는 --save-dev 의 약어입니다.
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,10 @@ module.exports = {
     },
     // 파일 에러가 어디서 오나 찾아줍니다.
     devtool: 'inline-source-map',
-    // 
+    // webpack-dev-server가 쓰는 host 주소입니다.
+    // 여기를 기준으로 localhost:8080으로 합니다.
+    // 이 설정을 안 해주면 index.html에서 bundle을 찾을 수 없다합니다.
+    // - 기본이 '.'이기 때문에, '.' 에서 찾기 때문입니다.
     devServer: {
         contentBase: './dist'
     },
@@ -26,5 +29,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
+        // 서버 스크립트(server.js)를 위해 사용합니다.
+        // 
+        publicPath: '/',
     },
 }
